7b07f905daf820b4f0d934f0f0498aaab3d53486
musou_aka <e155753@akahorikiichi-no-MacBook-Pro.local>
2017-10-22 01:33:31 +0900

    add ryukyu loop route draw

--- ARuke/ARuke/MapController.swift
+++ ARuke/ARuke/MapController.swift
@@ -8,14 +8,16 @@
 
 import Foundation
 import GoogleMaps
+import Alamofire
+import SwiftyJSON
 
 class MapConroller: UIViewController, CLLocationManagerDelegate,GMSMapViewDelegate {
     let locationManager = CLLocationManager()
     var mapView: GMSMapView!
     
     // WGS84の座標系での琉球大学の位置(緯度, 経度)
-    let latitude = 26.247582
-    let longitude = 127.765142
+    let ryukyuLatitude = 26.253726
+    let ryukyuLongitude = 127.766949
     let zoomLevel:Float = 17
     
     /** override vieDidLoad()
@@ -27,13 +29,13 @@ class MapConroller: UIViewController, CLLocationManagerDelegate,GMSMapViewDelega
         initMapView()
         setLocateManager()
         ryukyuLocationMarker()
-        
+        getRyukyuLoopRoutes()
     }
     
     // mapViewの初期化. 最初は琉球大学を写すよう指定
     func initMapView(){
         // WGS84の座標系でカメラを設定. latitude: 緯度, longitude: 経度
-        let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: zoomLevel)
+        let camera = GMSCameraPosition.camera(withLatitude: ryukyuLatitude, longitude: ryukyuLongitude, zoom: zoomLevel)
         
         // GoogleMapの初期化
         mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
@@ -63,20 +65,6 @@ class MapConroller: UIViewController, CLLocationManagerDelegate,GMSMapViewDelega
         locationManager.distanceFilter = 100
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
-    }
-    
-
-    
-    // 琉球大学の場所にmarkerを設置できる関数
-    func ryukyuLocationMarker(){
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        
-        // マーカーの場所を表示. WGS84の座標系. latitude: 緯度, longitude: 経度
-        marker.position = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        marker.title = "Ryuukyuu"
-        marker.snippet = "Okinawa"
-        marker.map = mapView
     }
     
     // flagがtrueの場合, 自分の位置を表示. 役割をわかりやすくするために関数化しただけ.
@@ -151,6 +139,102 @@ class MapConroller: UIViewController, CLLocationManagerDelegate,GMSMapViewDelega
         present(aleartController, animated:true, completion:nil)
         
     }
+    
+    
+    func getRyukyuLoopRoutes() {
+        
+        let requestURL = createRequestRyukyuURL()
+        print(requestURL)
+        Alamofire.request(requestURL).responseJSON { response in
+        
+            // そもそも通信が成功しているか(サーバーからの返答があるか)判定
+            switch response.result {
+            
+            case .success:
+                print(response.request?.url! as Any)
+                
+                let json = JSON(response.result.value!)
+                if json["status"].stringValue == "OK" {
+                    
+                    
+                    // 検索ルート候補のすべてを取得
+                    let routes = json["routes"].arrayValue
+                    // 検索ルート候補の0番目の始点から終点までのルートの情報を取得
+                    let overviewPolyline = routes[0]["overview_polyline"].dictionary
+                    let route = overviewPolyline!["points"]?.stringValue
+                    
+                    // ルートの線を引く
+                    let path = GMSPath.init(fromEncodedPath: route!)
+                    let polyline = GMSPolyline(path: path)
+                    
+                    polyline.strokeColor = .blue
+                    polyline.strokeWidth = 10.0
+                    polyline.map = self.mapView
+                }
+                
+            case .failure(let error):
+                print(error)
+                self.alertMessage(message: "通信ができないので, ルートを検索することができません.")
+                return
+                
+            }
+            
+        }
+        
+    }
+    
+    // 琉球大学のループ道路のルート検索URL
+    func createRequestRyukyuURL() -> String{
+        
+        // baseURLの作成.
+        let pearentURL = "https://maps.googleapis.com/maps/api/directions"
+        
+        
+        // 必須項目
+        let outputFormat="json" // json or xml
+        let origin = "\(ryukyuLatitude),\(ryukyuLongitude)"// 出発点の緯度と経度
+        let destination = "\(ryukyuLatitude),\(ryukyuLongitude)" // 到着点の緯度と経度
+        
+        // 省略可能な項目
+        let mode = "walking" //driving(default), walking, bicycling, transit
+        let waypoints = ["26.245985,127.763861", // 中継点.  琉球大学南口
+                         "26.247313,127.768448"] // 琉球大学東口
+                        .joined(separator: "|")
+        let alternatives = "false" // 複数のルートを表示 -> true , いらない -> false
+        
+        // 子URLの作成
+        let childURL = ["\(outputFormat)?",
+            "origin=\(origin)",
+            "destination=\(destination)",
+            "alternatives=\(alternatives)",
+            "mode=\(mode)",
+            "waypoints=\(waypoints)"]
+            .joined(separator: "&")
+        
+        // URLの結合
+        let requestURL = "\(pearentURL)/\(childURL)"
+        // パイプライン(|)のエンコード(これをしないと中継点が正しく認識されない.
+        if let requestEncodeURL = requestURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed){
+            return requestEncodeURL
+        }
+        
+        return requestURL
+        
+    }
+    
+    // 琉球大学の場所にmarkerを設置できる関数
+    func ryukyuLocationMarker(){
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        
+        // マーカーの場所を表示. WGS84の座標系. latitude: 緯度, longitude: 経度
+        marker.position = CLLocationCoordinate2D(latitude: ryukyuLatitude, longitude: ryukyuLongitude)
+        marker.title = "Ryuukyuu"
+        marker.snippet = "Okinawa"
+        marker.map = mapView
+    }
+    
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
