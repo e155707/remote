1826c604c1c4eafe82f1df404913f84701c14488
musou_aka <e155753@akahorikiichi-no-MacBook-Pro.local>
2017-11-08 12:27:27 +0900

    add MapDelegate.swift

---
+++ ARuke/ARuke/Map/MapDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  MapDelegate.swift
+//  ARuke
+//
+//  Created by 赤堀　貴一 on 2017/11/08.
+//  Copyright © 2017年 Ryukyu. All rights reserved.
+//
+
+import Foundation
+import GoogleMaps
+
+protocol MapDelegate{
+    
+}
