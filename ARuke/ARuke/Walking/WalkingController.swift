d6bc6fe1055e55468ccc12c213495a69e8fa4d77
e155707 <e155707@ie.u-ryukyu.ac.jp>
2017-11-20 10:59:44 +0900

    add walking

--- ARuke/ARuke/Walking/WalkingController.swift
+++ ARuke/ARuke/Walking/WalkingController.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2017年 Ryukyu. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SceneKit
+import ARKit
+
+class WalkingController: UIViewController, ARSCNViewDelegate{
+    @IBOutlet weak var WalkingDescriptionLabel: UILabel!
+    @IBOutlet weak var characterDisplay: SCNView!
+    
+    let character:ManageCharacter = ManageCharacter()
+    var characterScene:SCNScene = SCNScene()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        characterScene = character.managerCamera()
+        
+        character.managerCharacter(characterScene)
+        characterDisplay.scene = characterScene
+        
+        WalkingDescriptionLabel.text = "指定した　チェックポイントに向かって、\n戦いに　いどむんやで\n\n...歩きスマホは　アカンやで！！"
+        WalkingDescriptionLabel.numberOfLines = 0
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
+
+
+
