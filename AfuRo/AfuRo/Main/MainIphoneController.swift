850de42f859fc1e270aa1d8821d2043626ec1f44
e155707 <e155707@ie.u-ryukyu.ac.jp>
2017-12-11 13:25:28 +0900

    fix

--- AfuRo/AfuRo/Main/MainIphoneController.swift
+++ AfuRo/AfuRo/Main/MainIphoneController.swift
@@ -10,9 +10,13 @@
 import UIKit
 import SceneKit
 import ARKit
+import MapKit
+import Social
 
-class MainIphoneController: UIViewController, ARSCNViewDelegate {
+class MainIphoneController: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
     
+    let cameraController = CameraController()
+    let login = Login()
     
     @IBOutlet var ARView: ARSCNView!
     //var ARView:ARSCNView?
@@ -26,6 +30,7 @@ class MainIphoneController: UIViewController, ARSCNViewDelegate {
     // アフロの大きさを調整するボタン
     @IBOutlet var plusButton: UIButton!
     @IBOutlet var minusButton: UIButton!
+    @IBOutlet var cameraButton: UIButton!
     
     enum ButtonTag: Int {
         case Right = 1
@@ -47,7 +52,7 @@ class MainIphoneController: UIViewController, ARSCNViewDelegate {
     
     // Afuroの増える大きさを調整する係数.
     // 今の計算式 アフロの大きさ = 1 + 歩数(totalStepsData) * afuroScaleCoeff
-    let afuroScaleCoeff:Float = 1;
+    let afuroScaleCoeff:Float = 0.001;
     
     let dataController = DataController()
     let healthDataController = HealthDataController()
@@ -68,30 +73,24 @@ class MainIphoneController: UIViewController, ARSCNViewDelegate {
         ARView.scene = scene
         initMoveButton();
         guard
-            let afuroScene = SCNScene(named: "art.scnassets/daefile/aforo.scn"),
+            let afuroScene = SCNScene(named: "art.scnassets/daefile/afuro.scn"),
             let afuroNode = afuroScene.rootNode.childNode(withName:"afuro" , recursively: true)
             else{ return }
         
-        // ヘルスケアのデータの取得
-        if healthDataController.checkAuthorization(){
-            // もしヘルスケアのアクセスがtrueなら, 今の時刻と最後に歩いた時刻の間の歩いた数の合計を取得.
-            healthStepsData = healthDataController.getStepsHealthKit(startDate: Date(), endDate: dataController.getLastDateData())
-        }else{
-            // // もしヘルスケアにアクセスできないなら, ダミー関数を取得.
-            healthStepsData = healthDataController.getDummyStepsData()
-        }
+       // setLocationManager()
         
         // これまでの歩数の取得
-        totalStepsData = dataController.getTotalStepsData() + healthStepsData
+        totalStepsData = dataController.getTotalStepsData() + getStepsHealthKit()
+        print("totalStepsDate =\(dataController.getTotalStepsData())")
         
         // アフロの位置
         afuroNode.position = SCNVector3(0,0,1)
         
         afuroNode.position = SCNVector3(0,0,-3)
         // アフロの回転
-        afuroNode.eulerAngles = SCNVector3(-90,0,0)
+        afuroNode.eulerAngles = SCNVector3(-100,0,0)
         
-        totalStepsData += Login().loginGetSteps()
+        totalStepsData += login.loginGetSteps()
         if Login().isDailyFirstLogin(dataController.getLastDateData()){
             //self.createSelectElementWindow()
         }
@@ -118,7 +117,7 @@ class MainIphoneController: UIViewController, ARSCNViewDelegate {
     // データを保存する関数.
     @objc func saveData(){
         dataController.setTotalStepsData(totalStepsData)
-        dataController.setLastDateData(Date())
+        
     }
     
     // アフロを移動させるボタンの設定.
@@ -167,6 +166,8 @@ class MainIphoneController: UIViewController, ARSCNViewDelegate {
         minusButton.tag = ButtonTag.Minus.rawValue;
         // タップされている間, moveNodeを呼ぶよう設定.
         minusButton.addTarget(self, action: #selector(self.touchButtonScale), for: .touchDown)
+        
+        cameraButton.addTarget(self, action: #selector(self.shutter), for: .touchDown)
     }
     
     // ボタンがタップされた時に呼び出されるメソッド
@@ -225,6 +226,10 @@ class MainIphoneController: UIViewController, ARSCNViewDelegate {
         default:
             return
         }
+    }
+    
+    @objc func shutter(){
+        cameraController.savePicture(ARView.snapshot())
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -292,6 +297,99 @@ class MainIphoneController: UIViewController, ARSCNViewDelegate {
     @objc func tapSelectElementWindow(){
         selectElementWindow = nil
         print("tap2")
+    }
+    
+    func getStepsHealthKit() -> Int{
+        let healthDataController = HealthDataController()
+        // ヘルスケアから取ってきた歩数を保存する変数
+        var healthStepsData = 0
+        // ヘルスケアのデータの取得
+        if healthDataController.checkAuthorization() {
+            // もしヘルスケアのアクセスがtrueなら, 今の時刻と最後に歩いた時刻の間の歩いた数の合計を取得.
+            healthStepsData = healthDataController.getStepsHealthKit(startDate: dataController.getLastDateData(), endDate: Date())
+            print("healthDate = \(healthStepsData)")
+        }else{
+            // // もしヘルスケアにアクセスできないなら, ダミー関数を取得.
+            healthStepsData = healthDataController.getDummyStepsData()
+        }
+        
+        return healthStepsData
+    }
+    
+    let locationManager = CLLocationManager()
+    var oldLocation: CLLocation?
+    
+    // 位置情報の設定
+    func setLocationManager(){
+        
+        // 現在位置情報の精度, 誤差を決定. 以下の様なものがある
+        // - kCLLocationAccuracyBestForNavigation ナビゲーションに最適な値
+        // - kCLLocationAccuracyBest 最高精度(iOS,macOSのデフォルト値)
+        // - kCLLocationAccuracyNearestTenMeters 10m
+        // - kCLLocationAccuracyHundredMeters 100m（watchOSのデフォルト値）
+        // - kCLLocationAccuracyKilometer 1Km
+        // - kCLLocationAccuracyThreeKilometers 3Km
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // 現在位置からどれぐらい動いたら更新するか. 単位はm
+        locationManager.distanceFilter = 10
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    // 位置情報がlocationManager.distanceFilterの値分更新された時に呼び出されるメソッド.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location: CLLocation = locations.last else { return }
+        
+        if oldLocation == nil {
+            oldLocation = location
+        }
+        
+        let locationDistance = location.distance(from: oldLocation!)
+        print("moveDistance = \(locationDistance)")
+        totalStepsData += Int(locationDistance)
+        saveData()
+        oldLocation = location
+        
+    }
+    
+    //起動時と, 位置情報のアクセス許可が変更された場合に呼び出されるメソッド. ここで位置情報のアクセス許可をとる.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            print("ユーザーはこのアプリケーションに関してまだ選択を行っていません")
+            // 位置情報アクセスを常に許可するかを問いかける
+            locationManager.requestAlwaysAuthorization()
+            
+            break
+            
+        case .denied:
+            print("ローケーションサービスの設定が「無効」になっています (ユーザーによって、明示的に拒否されています）")
+            // 位置情報アクセスを常に許可するかを問いかける
+            locationManager.requestAlwaysAuthorization()
+            // 「設定 > プライバシー > 位置情報サービス で、位置情報サービスの利用を許可して下さい」を表示する
+            ErrorController().notGetLocation()
+            break
+            
+        case .restricted:
+            print("このアプリケーションは位置情報サービスを使用できません(ユーザによって拒否されたわけではありません)")
+            // 「このアプリは、位置情報を取得できないために、正常に動作できません」を表示する
+            ErrorController().notGetLocation()
+            break
+            
+        case .authorizedAlways:
+            print("常時、位置情報の取得が許可されています。")
+            break
+            
+        case .authorizedWhenInUse:
+            print("起動時のみ、位置情報の取得が許可されています。")
+            
+            locationManager.requestAlwaysAuthorization()
+            // 位置情報取得の開始処理
+            break
+            
+        }
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
